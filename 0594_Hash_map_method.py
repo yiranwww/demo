69ae0af3c84116613bd41828d2be32600f551845
yiranwww <wyr9568@sina.com>
2025-07-01 04:23:01 +0800

    0594: using hash map to count the freq of each elements and check if the "num + 1" exist

---
+++ 0594_Hash_map_method.py
@@ -0,0 +1,16 @@
+from typing import List
+import collections
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        freq_map = collections.Counter(nums)
+        max_length = 0
+        for num, freq in freq_map.items():
+            if num + 1 in freq_map:
+                cur_length = freq_map[num] + freq_map[num+1]
+                max_length = max(max_length, cur_length)
+        return max_length
+    
+solution = Solution()
+nums = [1,3,2,2,5,2,3,7]
+ans = solution.findLHS(nums)
+print(ans)  # Output: 5
