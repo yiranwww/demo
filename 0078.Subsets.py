cce6a75bfcbd45f1600c0fb95ae247f8462bdd2f
yiranwww <wyr9568@sina.com>
2025-07-16 03:02:47 +0800

    0078. Backtrack

---
+++ 0078.Subsets.py
@@ -0,0 +1,13 @@
+class Solution:
+    def subsets(self, nums):
+        res = []
+
+        def backtrack(start, path):
+            res.append(path[:])
+            for i in range(start, len(nums)):
+                path.append(nums[i])
+                backtrack(i + 1, path)
+                path.pop()
+        
+        backtrack(0, [])
+        return res
