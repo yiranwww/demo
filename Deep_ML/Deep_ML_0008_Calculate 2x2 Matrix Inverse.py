3868b258b93f839266b5c0ce7d7bd06e811e0d57
yiranwww <wyr9568@sina.com>
2025-08-30 12:37:14 +0800

    add condition when matrix is not invertible

--- Deep_ML/Deep_ML_0008_Calculate 2x2 Matrix Inverse.py
+++ Deep_ML/Deep_ML_0008_Calculate 2x2 Matrix Inverse.py
@@ -10,17 +10,21 @@
 
 import numpy as np
 def inverse_2x2(matrix: list[list[float]]) -> list[list[float]]:
-    # matrix_np = np.array(matrix)
-    # a = matrix_np[0][0]
-    # b = matrix_np[0][1]
-    # c = matrix_np[1][0]
-    # d = matrix_np[1][1]
-    # numerator = 1/(a * d - b * c)
-    # inverse = [[0] * 2 for _ in range(2)]
-    # inverse[0][0] = round(d * numerator, 4)
-    # inverse[0][1] = round(-b * numerator, 4)
-    # inverse[1][0] = round(-c * numerator, 4)
-    # inverse[1][1] = round(a * numerator, 4)
+    matrix_np = np.array(matrix)
+    a = matrix_np[0][0]
+    b = matrix_np[0][1]
+    c = matrix_np[1][0]
+    d = matrix_np[1][1]
+
+    if a * d - b * c == 0:
+        return None
+    
+    numerator = 1/(a * d - b * c)
+    inverse = [[0] * 2 for _ in range(2)]
+    inverse[0][0] = round(d * numerator, 4)
+    inverse[0][1] = round(-b * numerator, 4)
+    inverse[1][0] = round(-c * numerator, 4)
+    inverse[1][1] = round(a * numerator, 4)
 
     # matrix_np = np.array(matrix)
     # inverse = np.linalg.inv(matrix_np)
