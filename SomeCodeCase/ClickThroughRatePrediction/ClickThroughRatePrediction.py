84bcf05e115c9191ccc1fd64717f8df722978c19
yiranwww <wyr9568@sina.com>
2025-09-05 14:22:00 +0800

    add latex format test sample

--- SomeCodeCase/ClickThroughRatePrediction/ClickThroughRatePrediction.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-import os
-
-base_path = os.path.dirname(__file__)  # folder of your script
-csv_path = os.path.join(base_path, "Data", "ad_10000records.csv")
-data = pd.read_csv(csv_path)
-print(data.head())
